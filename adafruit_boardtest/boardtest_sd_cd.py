58e8002e66fdf181cd4ba142695587b4ce851494
dherrada <dylan.herrada@adafruit.com>
2021-03-23 14:06:32 -0400

    removed main function and if __name__

--- adafruit_boardtest/boardtest_sd_cd.py
+++ adafruit_boardtest/boardtest_sd_cd.py
@@ -86,27 +86,3 @@ def run_test(pins, cd_pin=SD_CD_PIN_NAME):
     # Else (no pins found)
     print("No CD pin found")
     return NA, []
-
-
-def _main():
-
-    # List out all the pins available to us
-    pins = list(dir(board))
-    print()
-    print("All pins found:", end=" ")
-
-    # Print pins
-    for pin6 in pins:
-        print(pin6, end=" ")
-    print("\n")
-
-    # Run test
-    result = run_test(pins)
-    print()
-    print(result[0])
-    print("Pins tested: " + str(result[1]))
-
-
-# Execute only if run as main.py or code.py
-if __name__ == "__main__":
-    _main()
--- adafruit_boardtest/boardtest_voltage_monitor.py
+++ adafruit_boardtest/boardtest_voltage_monitor.py
@@ -89,27 +89,3 @@ def run_test(pins):
     # Else (no pins found)
     print("No battery monitor pins found")
     return NA, []
-
-
-def _main():
-
-    # List out all the pins available to us
-    pins = list(dir(board))
-    print()
-    print("All pins found:", end=" ")
-
-    # Print pins
-    for pin8 in pins:
-        print(pin8, end=" ")
-    print("\n")
-
-    # Run test
-    result = run_test(pins)
-    print()
-    print(result[0])
-    print("Pins tested: " + str(result[1]))
-
-
-# Execute only if run as main.py or code.py
-if __name__ == "__main__":
-    _main()
